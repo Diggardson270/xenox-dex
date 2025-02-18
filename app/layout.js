bb396abc977b32077fcd6df94ad8a82cd965516f
Aniyikaye Enoch <enochaniyikaye@gmail.com>
2025-02-18 03:24:57 +0100

    ...

--- app/layout.js
+++ app/layout.js
@@ -22,11 +22,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`scroll-smooth ${geistSans.variable} ${geistMono.variable} antialiased bg-gray-950 text-white`}
+        className={`scroll-smooth ${geistSans.variable} ${geistMono.variable} antialiased bg-gradient-to-r from-blue-900 via-purple-950 to-gray-950 bg-clip-border bg-opacity-80`}
       >
-        <Header />
-        {children}
-        <Footer />
+        <div className="relative overflow-hidden min-h-screen">
+          {/* Blob Glassmorphism Background */}
+          <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-blue-900 via-gray-900 to-purple-950 opacity-70 z-0"></div>
+          <div className="absolute top-0 left-0 w-full h-full backdrop-blur-3xl bg-black/80 z-10"></div>
+
+          <Header />
+          <div className="relative z-20">{children}</div>
+          <Footer />
+        </div>
       </body>
     </html>
   );
