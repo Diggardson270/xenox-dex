f286643e89bf67b409986cbcd1cc39b093b2aebf
Aniyikaye Enoch <enochaniyikaye@gmail.com>
2025-02-18 11:21:53 +0100

    ...

--- app/components/SwapPanel.jsx
+++ app/components/SwapPanel.jsx
@@ -78,7 +78,7 @@ function SwapPanel() {
                         setActiveTokenField("from");
                         setModalOpen(true);
                       }}
-                      className="text-xs lg:text-sm w-40 px-3 py-7 bg-gray-800 bg-opacity-45 text-gray-300 font-bold rounded-lg focus:outline-none flex items-center justify-center"
+                      className="text-xs lg:text-sm w-40 px-3 py-7 bg-gray-800 bg-opacity-45 text-gray-200 font-bold rounded-lg focus:outline-none flex items-center justify-center"
                     >
                       <div className="flex items-center mr-2">
                         <Image
@@ -90,7 +90,7 @@ function SwapPanel() {
                         />
                         {fromToken.name}
                       </div>
-                      <ChevronDown size={16} className="text-gray-300" />
+                      <ChevronDown size={16} className="text-gray-200" />
                     </button>
                   </div>
                   <div className="w-1/2">
@@ -100,7 +100,7 @@ function SwapPanel() {
                       placeholder="0"
                       value={fromAmount}
                       onChange={(e) => setFromAmount(e.target.value)}
-                      className="w-full text-3xl text-right p-2 rounded bg-transparent text-gray-300 focus:outline-none"
+                      className="w-full text-3xl text-right p-2 rounded bg-transparent text-gray-200 focus:outline-none"
                     />
                     <div className="flex flex-col items-end pr-6">
                       <p className="text-slate-500 font-bold">~$0</p>
@@ -122,9 +122,9 @@ function SwapPanel() {
               >
                 {/* Conditionally render the icon based on isSwapped state */}
                 {isSwapped ? (
-                  <ArrowDownUp size={14} className="text-gray-300" />
+                  <ArrowDownUp size={14} className="text-gray-200" />
                 ) : (
-                  <ArrowUpDown size={14} className="text-gray-300" />
+                  <ArrowUpDown size={14} className="text-gray-200" />
                 )}
               </div>
             </div>
@@ -150,9 +150,9 @@ function SwapPanel() {
                         setActiveTokenField("to");
                         setModalOpen(true);
                       }}
-                      className="text-xs lg:text-sm w-40 px-3 py-7 bg-gray-800 bg-opacity-45 text-gray-300 font-bold rounded-lg focus:outline-none flex items-center justify-center"
+                      className="text-xs lg:text-sm w-40 px-3 py-7 bg-gray-800 bg-opacity-45 text-gray-200 font-bold rounded-lg focus:outline-none flex items-center justify-center"
                     >
-                      <div className="flex items-center mr-2 text-gray-300">
+                      <div className="flex items-center mr-2 text-gray-200">
                         <Image
                           src={toToken.logo}
                           alt={toToken.name}
@@ -162,7 +162,7 @@ function SwapPanel() {
                         />
                         {toToken.name}
                       </div>
-                      <ChevronDown size={16} className="text-gray-300" />
+                      <ChevronDown size={16} className="text-gray-200" />
                     </button>
                   </div>
                   <div className="w-1/2">
@@ -172,7 +172,7 @@ function SwapPanel() {
                       placeholder="0"
                       value={toAmount}
                       onChange={(e) => setToAmount(e.target.value)}
-                      className="w-full text-3xl text-right p-2 rounded bg-transparent text-gray-300 focus:outline-none"
+                      className="w-full text-3xl text-right p-2 rounded bg-transparent text-gray-200 focus:outline-none"
                     />
                     <div className="flex flex-col items-end pr-6">
                       <p className="text-slate-500 font-bold">~$0</p>
@@ -193,13 +193,13 @@ function SwapPanel() {
               ></div>
               {/* Modal Content */}
               <div className="w-[90%] bg-gray-950 p-4 rounded-lg z-10 max-w-sm">
-                <h2 className="text-xl text-gray-300 mb-4">Select a token</h2>
+                <h2 className="text-xl text-gray-200 mb-4">Select a token</h2>
                 <ul>
                   {tokens.map((token) => (
                     <li key={token.name} className="mb-2  flex items-center">
                       <button
                         type="button"
-                        className="w-full h-20 flex items-center justify-center text-left px-4 py-2 bg-gray-900 text-gray-300 hover:bg-gray-600 rounded"
+                        className="w-full h-20 flex items-center justify-center text-left px-4 py-2 bg-gray-900 text-gray-200 hover:bg-gray-600 rounded"
                         onClick={() => {
                           if (activeTokenField === "from") {
                             setFromToken(token);
@@ -228,7 +228,7 @@ function SwapPanel() {
 
         {/* RIGHT: Instruction Card */}
         <div className="bg-gray-900 rounded-3xl p-6 lg:w-2/4 mx-auto">
-          <h2 className="text-2xl text-gray-300 mb-6 text-left">Swap</h2>
+          <h2 className="text-2xl text-gray-200 mb-6 text-left">Swap</h2>
           <div className="relative">
             {/* Vertical line behind the numbered circles */}
             <div className="absolute left-4 top-8 bottom-0 w-px bg-gray-950"></div>
@@ -236,12 +236,12 @@ function SwapPanel() {
               {instructions.map((text, index) => (
                 <li key={index} className="flex items-start">
                   <div className="relative flex-shrink-0">
-                    <span className="flex items-center justify-center w-8 h-8 rounded-full text-sm text-gray-300 font-bold bg-gray-950">
+                    <span className="flex items-center justify-center w-8 h-8 rounded-full text-sm text-gray-200 font-bold bg-gray-950">
                       {index + 1}
                     </span>
                   </div>
                   <div className="ml-4">
-                    <p className="text-gray-300">{text}</p>
+                    <p className="text-gray-200">{text}</p>
                   </div>
                 </li>
               ))}
