db3a5d14d5f93dc50c5bbbcbadb7aa738e592663
Aniyikaye Enoch <enochaniyikaye@gmail.com>
2025-02-19 14:06:32 +0100

    styled the tokenSelctor Modal

--- app/components/TokenSelector.jsx
+++ app/components/TokenSelector.jsx
@@ -1,10 +1,10 @@
-// components/TokenSelector.js
 "use client";
 
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import { useTokens } from "../context/TokenContext";
 import sol from "../../public/solana-sol-logo.svg";
+import { Search, X } from "lucide-react";
 
 export default function TokenSelector({
   modalOpen,
@@ -13,21 +13,32 @@ export default function TokenSelector({
   setFromToken,
   setToToken,
   setFromAddress,
-  setToAddress
+  setToAddress,
 }) {
   const { tokens, loading } = useTokens();
   const [searchQuery, setSearchQuery] = useState("");
+
+  // Disable scrolling when modal is open
+  useEffect(() => {
+    if (modalOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [modalOpen]);
 
   // Filter tokens based on search query
   const filteredTokens = tokens.filter((token) =>
     token.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
-  function pasrseLogoURI(logo){
+  function pasrseLogoURI(logo) {
     if (!logo) {
-      return sol
+      return sol;
     }
-  
     if (!logo.startsWith("https://")) {
       return `https://${logo}`;
     }
@@ -45,48 +56,63 @@ export default function TokenSelector({
       ></div>
 
       {/* Modal Content */}
-      <div className="w-[90%] bg-gray-950 p-4 rounded-lg z-10 max-w-sm">
-        <h2 className="text-xl text-gray-200 mb-4">Select a token</h2>
+      <div className="relative w-[90%] bg-gray-900 px-4 py-10 lg:px-10 rounded-lg z-10 max-w-xl">
+        {/* Close Icon */}
+        <div
+          className="absolute top-11 right-11 cursor-pointer"
+          onClick={() => setModalOpen(false)}
+        >
+          <X size={24} className="text-gray-200" />
+        </div>
+
+        <h2 className="text-xl text-gray-200 mb-14">Select a token</h2>
 
         {/* Search Bar */}
-        <input
-          type="text"
-          placeholder="Search token..."
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          className="w-full p-2 mb-2 border rounded bg-gray-800 text-gray-200 focus:outline-none"
-        />
+        <div className="relative">
+          <Search
+            size={20}
+            className="absolute left-3 top-7 transform -translate-y-1/2 text-gray-400"
+          />
+          <input
+            type="text"
+            placeholder="SOL, USDC, JLP...."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className="w-full pl-10 pr-4 py-4 mb-2 rounded-lg border border-gray-700 bg-transparent text-gray-500 placeholder:text-gray-700 outline-none focus:border-gray-500"
+          />
+        </div>
 
         {loading ? (
           <p className="text-gray-400 text-center">Loading tokens...</p>
         ) : (
-          <ul className="overflow-auto max-h-[400px]">
+          <ul className="overflow-auto max-h-[400px] mt-10 py-5">
             {filteredTokens.map((token) => (
-              <li key={token.address} className="mb-2 flex items-center">
+              <li
+                key={token.address}
+                className="mb-2 flex items-center justify-center"
+              >
                 <button
                   type="button"
-                  className="w-full h-20 flex items-center justify-start px-4 py-2 bg-gray-900 text-gray-200 hover:bg-gray-600 rounded"
+                  className="w-[90%] h-20 flex items-center justify-between px-4 lg:py-8 py-2 bg-gray-700 bg-opacity-15 text-gray-200 hover:bg-gray-800 rounded"
                   onClick={() => {
                     if (activeTokenField === "from") {
                       setFromToken(token);
-                      setFromAddress(token.address)
+                      setFromAddress(token.address);
                     } else if (activeTokenField === "to") {
                       setToToken(token);
-                      setToAddress(token.address)
+                      setToAddress(token.address);
                     }
                     setModalOpen(false);
                   }}
                 >
-                  {/* {token.localImagePath && ( */}
-                    <Image
-                      src={pasrseLogoURI(token.logoURI)}
-                      alt={token.symbol}
-                      width={32}
-                      height={32}
-                      className="mr-2 rounded-full"
-                    />
-                  {/* )} */}
                   {token.symbol} ({token.name})
+                  <Image
+                    src={pasrseLogoURI(token.logoURI)}
+                    alt={token.symbol}
+                    width={32}
+                    height={32}
+                    className="mr-2 rounded-full"
+                  />
                 </button>
               </li>
             ))}
