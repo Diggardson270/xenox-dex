08ee9832925890294db7f4e0e26393969b68815c
Aniyikaye Enoch <enochaniyikaye@gmail.com>
2025-02-21 15:25:09 +0100

    footer styling

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,13 +2,14 @@
 
 import React from "react";
 import Link from "next/link";
+import { Lock, FileText, Mail } from "lucide-react";
 
 function Footer() {
   return (
     <footer className="footerbg bg-gray-950 border border-gray-800 rounded-t-3xl w-[90%] z-50 max-w-7xl mx-auto px-6 md:px-8 py-6 text-center text-gray-300 relative shadow-lg">
       <div className="absolute inset-0 bg-gray-950 bg-opacity-75 rounded-t-3xl -z-10"></div>
-      <div className="container mx-auto flex flex-col items-center space-y-6">
-        <p className="max-w-2xl text-xs lg:text-lg font-medium leading-relaxed">
+      <div className="mx-auto text-gray-400 flex flex-col items-center space-y-6">
+        <p className="max-w-2xl text-xs md:text-sm lg:text-lg font-light leading-relaxed tracking-widest">
           XENOX DEX is the fastest DEX on SOL with built-in sniper protection.
           Experience super-fast token swaps and secure transactions. To
           celebrate our launch, the first 50 users to connect will receive 9000
@@ -16,14 +17,26 @@ function Footer() {
         </p>
 
         <div className="flex space-x-6 text-xs md:text-sm lg:text-base font-medium">
-          <Link href="/privacy" className="hover:text-gray-400 transition-all">
-            Privacy Policy
+          <Link
+            href="/privacy"
+            className="flex items-center space-x-1 hover:text-gray-400 transition-all"
+          >
+            <Lock className="w-3 h-3 md:w-5 md:h-5" />
+            <span>Privacy Policy</span>
           </Link>
-          <Link href="/terms" className="hover:text-gray-400 transition-all">
-            Terms of Service
+          <Link
+            href="/terms"
+            className="flex items-center space-x-1 hover:text-gray-400 transition-all"
+          >
+            <FileText className="w-3 h-3 md:w-5 md:h-5" />
+            <span>Terms of Service</span>
           </Link>
-          <Link href="/contact" className="hover:text-gray-400 transition-all">
-            Contact
+          <Link
+            href="/contact"
+            className="flex items-center space-x-1 hover:text-gray-400 transition-all"
+          >
+            <Mail className="w-3 h-3 md:w-5 md:h-5" />
+            <span>Contact</span>
           </Link>
         </div>
 
